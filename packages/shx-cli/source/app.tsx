06e21040eb19edab18ebfe9c5152ff3b1edd206b
rajikaimal <inbox.rajika@gmail.com>
2024-05-04 16:50:23 +0800

    spinner, improve readme

--- packages/shx-cli/source/app.tsx
+++ packages/shx-cli/source/app.tsx
@@ -1,5 +1,6 @@
 import {Text} from 'ink';
-import React, {useEffect} from 'react';
+import Spinner from 'ink-spinner';
+import React, {useEffect, useState} from 'react';
 import {setup} from './setup.js';
 import {useShxApi} from './use-shx-api.js';
 
@@ -16,6 +17,7 @@ type Props = {
 export default function App({config, message}: Props) {
 	const {url, authToken} = config;
 	const {reply, callWokerAI} = useShxApi({message});
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		if (url && authToken) {
@@ -26,9 +28,17 @@ export default function App({config, message}: Props) {
 		}
 	}, [url, authToken, message]);
 
-	return (
-		<>
-			<Text color="green">{reply.map(r => r)}</Text>
-		</>
-	);
+	useEffect(() => {
+		if (reply?.length) setIsLoading(false);
+	}, [reply]);
+
+	const render = () => {
+		if (isLoading) {
+			return <Spinner type="dots" />;
+		} else {
+			return <Text color="green">{reply.map(r => r)}</Text>;
+		}
+	};
+
+	return <>{render()}</>;
 }
